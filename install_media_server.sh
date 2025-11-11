64069074066c8c1b42b360b968322e9880ed8b7a
zykid <58476827+zykid@users.noreply.github.com>
2025-11-11 18:25:18 +0800

    Update install_media_server.sh

--- install_media_server.sh
+++ install_media_server.sh
@@ -1,1 +1,13 @@
+CONFIG_FILE="$HOME/.zykid_installer/config.json"
+if [ -f "$CONFIG_FILE" ]; then
+  BOT_TOKEN=$(jq -r '.BOT_TOKEN' "$CONFIG_FILE")
+  CHAT_ID=$(jq -r '.CHAT_ID' "$CONFIG_FILE")
+  NAS_PATH=$(jq -r '.NAS_PATH' "$CONFIG_FILE")
+  NAS_USER=$(jq -r '.NAS_USER' "$CONFIG_FILE")
+  NAS_PASS=$(jq -r '.NAS_PASS' "$CONFIG_FILE")
+  MOUNT_POINT=$(jq -r '.MOUNT_POINT' "$CONFIG_FILE")
+else
+  echo "请先创建配置文件：$CONFIG_FILE"
+  exit 1
+fi
 
